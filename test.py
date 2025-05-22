24615b37854d26349ffadbfd5ef58d140ab47ffc
yuuuuuzii <dh6030108@gmail.com>
2025-05-22 22:48:01 +0800

    HI

---
+++ test.py
@@ -0,0 +1,12 @@
+import gymnasium as gym
+
+env = gym.make("HalfCheetah-v4")
+obs, _ = env.reset()
+total_reward = 0
+for _ in range(1000):
+    action = env.action_space.sample()
+    obs, reward, done, truncated, info = env.step(action)
+    if done or truncated:
+        obs, _ = env.reset()
+    total_reward += reward
+print(total_reward)
